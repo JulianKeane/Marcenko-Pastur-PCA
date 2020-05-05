9ddf8404d29cd5b0de6edbc440f943cc60558b23
Julian Keane <julian.e.keane@gmail.com>
2020-05-05 12:30:35 -0400

    Adding identity function

--- MarcenkoPastur.jl
+++ MarcenkoPastur.jl
@@ -5,6 +5,20 @@ struct MarcenkoPastur{T<:UnivariateDistribution}
     rho::Real
 end
 
+# Indicator function 1(x)
+# Parameters
+#   x::Real     Real number of which we are evaluating 1(x) at
+#   a::Real     Lower bound of our set
+#   b::Real     Upper bound of our set
+function indicator(x::Real, a::Real, b::Real)
+    if a <= x <= b
+        return 1
+    else
+        return 0
+end
+
 alpha(rho) = (1-√rho)^2
 beta(rho) = (1+√rho)^2
-pdf(d::UnivariateDistribution, x::Real) = √((beta(rho)-x)(x-alpha(rho)))//(2πx)
+function pdf(d::UnivariateDistribution, x::Real)
+    √((beta(rho)-x)(x-alpha(rho)))//(2πx)*indicator(x, alpha(rho),beta(rho))
+end
